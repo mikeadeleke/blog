b42a35a670f2ab98232a6ecfdf3dc496920002cf
Mike Adeleke <madeleke13@gmail.com>
2013-09-27 12:21:08 -0500

    Finished post and now on comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,9 +20,26 @@ class PostsController < ApplicationController
   def index
   	@posts = Post.all
   end
-  
+
   def edit
     @post = Post.find(params[:id])
+  end
+
+  def update
+  	@post = Post.find(params[:id])
+
+  	if @post.update(params[:post].permit(:title, :text))
+  		redirect_to @post
+  	else
+  		render 'edit'
+  	end
+  end
+
+  def destroy
+  	@post = Post.find(params[:id])
+  	@post.destroy
+
+  	redirect_to posts_path
   end
 
   private
